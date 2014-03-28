e040f8d69986b97ce05f6ec43fec80c4405fd9b0
Oliver Rosales <jrosales@village88.com>
2014-03-28 14:59:14 -0700

    Haml Basic Assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Basic::Application.config.secret_key_base = '3a0c5de4458c7084cbda06603790346d56df3a08e2179b93a7935c2923dbb5337da828281220536a5b9993657f99bba9fbb72bdf04e65b76fcbdceef6070cd1a'
